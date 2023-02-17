e424e93f15c433d9864f82a6deb0e8510fed6cfb
natalia-pavlenko <npavl56@gmail.com>
2023-02-17 11:45:07 +0200

    localstorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,10 +7,10 @@ import { AppText, AppDiv } from './App.styled';
 export class App extends Component {
   state = {
     contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      //  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      //  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      //  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      //  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
@@ -22,7 +22,7 @@ export class App extends Component {
   componentDidMount() {
     const saveContacts = localStorage.getItem('saveContacts');
     const parsContacts = JSON.parse(saveContacts);
-    this.setState({ contacts: parsContacts });
+    if (parsContacts) {this.setState({ contacts: parsContacts })};
   }
 
   handleSubmit = ({ name, number, id }) => {
